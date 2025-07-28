838d19e83674d24f2f2ad971c2f0971f20a3ec1e
Amr Mousa <amr622414@gamil.com>
2025-07-28 21:36:14 +0300

    customer service ChatBot

--- src/app/Layout/Home/home-layout/home-layout.ts
+++ src/app/Layout/Home/home-layout/home-layout.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Nav } from "../../../Shared/Components/nav/nav";
 import { Hero } from "../../../Shared/Components/hero/hero";
 import { Services } from "../components/services/services";
@@ -8,6 +8,8 @@ import { Testimonial } from "../components/testimonial/testimonial";
 import { ReadyBook } from "../components/ready-book/ready-book";
 import { Faq } from "../components/faq/faq";
 import { Footer } from "../../../Shared/Components/footer/footer";
+import '@n8n/chat/style.css';
+import { createChat } from '@n8n/chat';
 
 @Component({
   selector: 'app-home-layout',
@@ -16,6 +18,41 @@ import { Footer } from "../../../Shared/Components/footer/footer";
   templateUrl: './home-layout.html',
   styleUrl: './home-layout.scss'
 })
-export class HomeLayout {
+export class HomeLayout implements OnInit {
 
+
+  ngOnInit(): void {
+
+
+  createChat({
+  webhookUrl: 'https://amrmousa1411.app.n8n.cloud/webhook/ded3971a-b4d6-436d-a107-ab02ae971ade/chat',
+  webhookConfig: {
+    method: 'POST',
+    headers: {}
+  },
+  target: '#n8n-chat',
+  mode: 'window',
+  chatInputKey: 'chatInput',
+  chatSessionKey: 'sessionId',
+  loadPreviousSession: true,
+  metadata: {},
+  showWelcomeScreen: false,
+  defaultLanguage: 'en',
+  initialMessages: [
+    'Welcome to Sanad 🚑',
+    'How can I assist you with our non-emergency ambulance services today?'
+  ],
+  i18n: {
+    en: {
+      title: 'Welcome to Sanad 🚑',
+      subtitle: "We're here to help with all your non-emergency ambulance needs.",
+      footer: '',
+      getStarted: 'Start New Chat',
+      inputPlaceholder: 'Type your question about Sanad services...',
+      closeButtonTooltip: 'Close chat'
+    }
+  }
+  });
+
+  }
 }
